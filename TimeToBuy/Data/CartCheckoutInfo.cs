d6d44bb79ba3284f7b7922cd1548d4485157e9b4
Jon <jon@jonhilton.net>
2025-05-07 21:51:11 +0100

    Testing a fix for validation of complex type

--- TimeToBuy/Data/CartCheckoutInfo.cs
+++ TimeToBuy/Data/CartCheckoutInfo.cs
@@ -4,6 +4,7 @@ namespace TimeToBuy.Data;
 
 public record CartCheckoutInfo
 {
+    [ValidateComplexType]
     public Address DeliveryAddress { get; set; } = new Address();
 
     [Required] 
@@ -11,17 +12,18 @@ public record CartCheckoutInfo
     [Required] public string CardHolderName { get; set; }
     [Required] public string CardSecurityNumber { get; set; }
     public DateTime CardExpiration { get; set; } = DateTime.Now.Date;
+ 
+}
 
-    public record Address
-    {
-        [Required] public string BuyerName { get; set; }
+public record Address
+{
+    [Required] public string BuyerName { get; set; }
 
-        [Required] public string Line1 { get; set; }
+    [Required] public string Line1 { get; set; }
 
-        [Required] public string Line2 { get; set; }
+    [Required] public string Line2 { get; set; }
 
-        [Required] public string City { get; set; }
+    [Required] public string City { get; set; }
 
-        [Required] public string PostCode { get; set; }
-    }
+    [Required] public string PostCode { get; set; }
 }
