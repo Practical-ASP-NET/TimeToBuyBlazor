e702bc25aa10e7c525dacf9f8f418bcb0ae6583c
Jon <jon@jonhilton.net>
2025-05-07 21:54:22 +0100

    Moved Address record back to where it was originally

--- TimeToBuy/Data/CartCheckoutInfo.cs
+++ TimeToBuy/Data/CartCheckoutInfo.cs
@@ -4,26 +4,23 @@ namespace TimeToBuy.Data;
 
 public record CartCheckoutInfo
 {
-    [ValidateComplexType]
-    public Address DeliveryAddress { get; set; } = new Address();
+    [ValidateComplexType] public Address DeliveryAddress { get; set; } = new Address();
 
-    [Required] 
-    public string CardNumber { get; set; }
+    [Required] public string CardNumber { get; set; }
     [Required] public string CardHolderName { get; set; }
     [Required] public string CardSecurityNumber { get; set; }
     public DateTime CardExpiration { get; set; } = DateTime.Now.Date;
- 
-}
 
-public record Address
-{
-    [Required] public string BuyerName { get; set; }
+    public record Address
+    {
+        [Required] public string BuyerName { get; set; }
 
-    [Required] public string Line1 { get; set; }
+        [Required] public string Line1 { get; set; }
 
-    [Required] public string Line2 { get; set; }
+        [Required] public string Line2 { get; set; }
 
-    [Required] public string City { get; set; }
+        [Required] public string City { get; set; }
 
-    [Required] public string PostCode { get; set; }
+        [Required] public string PostCode { get; set; }
+    }
 }
